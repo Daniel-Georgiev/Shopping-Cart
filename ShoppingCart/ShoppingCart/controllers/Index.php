83196b0d777d97616e2f4eafbac41e849706dd80
daniel-georgiev <daniko9505@gmail.com>
2015-09-30 01:00:02 +0300

    Views finished

--- ShoppingCart/ShoppingCart/controllers/Index.php
+++ ShoppingCart/ShoppingCart/controllers/Index.php
@@ -2,6 +2,10 @@
 namespace Controllers;
 class Index{
     public function index(){
+
+        $val = new \Framework\Validation();
+        $val->setRule('url', 'http://sada.c/')->setRule('minlength','http://sa.c/',100);
+        var_dump($val->validate());
        $view = \Framework\View::getInstance();
         $view->appendToLayout('body','admin.index');
         $view->display('layouts.default', array('c'=>array(1,2,3,4)),false);
